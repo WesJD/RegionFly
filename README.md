7a39e8cd2cf0f9ceb277fdb7e34e367966f91cba
WesJD <wesjavadev@gmail.com>
2017-02-12 20:11:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RegionFly
+A simple bukkit plugin for players to fly when they enter a specified region.
+
+## How to use
+* Install the plugin
+* Setup the config.yml with your regions
+* Restart the server
+
+## [Download](https://github.com/WesJD/RecursiveItems/releases)
---
+++ src/main/java/net/wesjd/regionfly/RegionFly.java
@@ -0,0 +1,49 @@
+package net.wesjd.regionfly;
+
+import com.sk89q.worldguard.bukkit.WGBukkit;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public class RegionFly extends JavaPlugin implements Listener {
+
+    private List<String> regions;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        regions = getConfig().getStringList("regions");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        final Location to = e.getTo();
+        final Location from = e.getFrom();
+        if(from.getBlockX() != to.getBlockX() || from.getBlockY() != to.getBlockY() || from.getBlockZ() != to.getBlockZ()) {
+            final Player player = e.getPlayer();
+            if(player.getGameMode() != GameMode.CREATIVE) {
+                final boolean wasFlying = player.isFlying();
+                final boolean fly = isInARegion(to);
+                player.setAllowFlight(fly);
+                player.setFlying(fly);
+                if(fly && !wasFlying) player.teleport(player.getLocation().add(0, 0.2, 0));
+            }
+        }
+    }
+
+    private boolean isInARegion(Location location) {
+        for(final ProtectedRegion region : WGBukkit.getRegionManager(location.getWorld()).getApplicableRegions(location))
+            if(regions.stream().anyMatch(name -> region.getId().equals(name))) return true;
+        return false;
+    }
+
+}
