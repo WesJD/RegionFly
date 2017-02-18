68c5cbe53f93da218dd458404b7d03947b305187
WesJD <wesjavadev@gmail.com>
2017-02-17 21:36:36 -0500

    Change to metadata to prevent conflict

--- src/main/java/net/wesjd/regionfly/RegionFly.java
+++ src/main/java/net/wesjd/regionfly/RegionFly.java
@@ -8,12 +8,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
 public class RegionFly extends JavaPlugin implements Listener {
 
+    private final FixedMetadataValue METADATA_VALUE = new FixedMetadataValue(this, "ok");
     private List<String> regions;
 
     @Override
@@ -29,16 +31,19 @@ public class RegionFly extends JavaPlugin implements Listener {
         final Location from = e.getFrom();
         if(from.getBlockX() != to.getBlockX() || from.getBlockY() != to.getBlockY() || from.getBlockZ() != to.getBlockZ()) {
             final Player player = e.getPlayer();
-
             if(player.getGameMode() != GameMode.CREATIVE) {
                 final boolean wasFlying = player.isFlying();
-                final boolean fly = isInARegion(to);
-
-                // player has walked into/out of a region
-                if(player.getAllowFlight() != fly) {
-                    player.setAllowFlight(fly);
-                    player.setFlying(fly);
-                    if(fly && !wasFlying) player.teleport(player.getLocation().add(0, 0.2, 0));
+                if(!isInARegion(to)) {
+                    if(player.hasMetadata("regionfly")) {
+                        player.setFlying(false);
+                        player.setAllowFlight(false);
+                        player.removeMetadata("regionfly", this);
+                    }
+                } else if(!player.hasMetadata("regionfly")) {
+                    player.setAllowFlight(true);
+                    player.setFlying(true);
+                    if(!wasFlying) player.teleport(player.getLocation().add(0, 0.2, 0));
+                    player.setMetadata("regionfly", METADATA_VALUE);
                 }
             }
         }
